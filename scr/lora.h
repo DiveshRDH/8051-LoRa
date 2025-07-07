2c2fb00933026d96d4085fc89f33e265fe242bf5
Rajbhar Divesh <84081602+DiveshRDH@users.noreply.github.com>
2025-07-07 11:10:02 +0530

    Add initial library source

---
+++ scr/lora.h
@@ -0,0 +1,10 @@
+#ifndef __LORA_H__
+#define __LORA_H__
+
+bit LoRa_Check(void);
+void LoRa_Reset(void);
+void LoRa_Init(void);
+void LoRa_Send(char* msg, unsigned char len);
+unsigned char LoRa_Receive(char* buf, unsigned char maxLen);
+
+#endif
